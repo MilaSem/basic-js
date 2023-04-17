b0d3c5bdf85ec3950781a02192370722d3ae6be7
MilaSem <milafoxsem@gmail.com>
2023-04-17 08:40:00 +0300

    feat: complete transform-array & common character count

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function getCommonCharacterCount(s1, s2) {
+  let tempStr1 = s1;
+  let tempStr2 = s2;
+
+  // перебираю символы во второй строке и смотрю, есть ли такие в первой
+  for (let i = 0; i < tempStr2.length; i++) {
+    if (tempStr1.includes(tempStr2[i])) {
+      console.log(tempStr2[i]);
+      tempStr1 = tempStr1.replace(tempStr2[i], '*'); //заменяем первое совпадение на '*' и переписываем tempStr1
+    }
+  }
+
+  let countAstr = 0; //считаю число звёздочек
+  for (let i = 0; i < tempStr1.length; i++) {
+    if (tempStr1[i] === '*') {
+      countAstr++;
+    }
+  }
+  
+  return countAstr;
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,48 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) { //массив по типу object, поэтому проверка через isArray
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  let tempArr = arr.concat(); //создаю новый массив, чтобы функция не влияла на исходный
+
+  let doubleNextStr = '--double-next';
+  let doubleNextIndStr = tempArr.indexOf(doubleNextStr);
+  if ((doubleNextIndStr != -1) && (tempArr[doubleNextIndStr + 1])) {
+      tempArr.splice(doubleNextIndStr + 1, 0, tempArr[doubleNextIndStr + 1]); //добавляю элемент перед doubleNextStr
+  }
+
+  let discNextStr = '--discard-next';    
+  let discNextIndStr = tempArr.indexOf(discNextStr);
+  if ((discNextIndStr != -1) && (tempArr[discNextIndStr + 1])) {
+      tempArr.splice(discNextIndStr + 1, 1); //удаляю элемент после discNextIndStr
+  }
+
+  let doublePrevStr = '--double-prev';
+  let doublePrevIndStr = tempArr.indexOf(doublePrevStr);
+  if ((doublePrevIndStr != -1) && (tempArr[doublePrevIndStr - 1])) {
+      tempArr.splice(doublePrevIndStr - 1, 0, tempArr[doublePrevIndStr - 1]); //добавляю элемент перед doubleNextStr
+  }
+
+  let discPrevStr = '--discard-prev';
+  let discPrevIndStr = tempArr.indexOf(discPrevStr);
+  if ((discPrevIndStr != -1) && (tempArr[discPrevIndStr - 1])) {
+      tempArr.splice(discPrevIndStr - 1, 1); //удаляю элемент перед discPrevStr
+  }
+
+  //убираю управляющие последовательности
+  let arrResult = [];
+
+  for (let i = 0; i < tempArr.length; i++) {
+      if ((tempArr[i] !== discNextStr) && (tempArr[i] !== discPrevStr) && (tempArr[i] !== doubleNextStr) && (tempArr[i] !== doublePrevStr)) {
+          arrResult.push(tempArr[i]);
+      }
+  }
+
+  return arrResult;
+
 }
 
 module.exports = {
